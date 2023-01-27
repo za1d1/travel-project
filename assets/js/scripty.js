e7873eb18d9866cb9096c95bdf013a8d9e36a371
za1d1 <zaidshanan899@gmail.com>
2023-01-27 17:47:21 +0300

    TRAVEL
    $

---
+++ assets/js/scripty.js
@@ -0,0 +1,62 @@
+// document.getElementById("sum").innerHTML = 3+2;
+// document.getElementById("min").innerHTML = 3-2;
+// document.getElementById("multi").innerHTML = 3*2;
+// document.getElementById("dev").innerHTML = 3/2;
+//////////////////////////////////////////////////
+
+/// V A R I A B L E S //
+
+//var identifier = value ;
+// var num1 = 5
+// var num2 = 10
+// console.log(num1 * num2);
+
+// num1 = ++num1
+// document.write(num1 + 1);
+// document.write(num1);
+// document.write('<br>')
+
+// document.write(` نتيجة المتغير هي ${num2} `)
+
+//////////////////////////////////////////////////
+// document.getElementById('sum').innerHTML = `6+10=${num1 + num2}`;
+// document.getElementById('min').innerHTML = `6-10=${num1 - num2}`;
+// document.getElementById('multi').innerHTML = `6*10=${num1 * num2}`;
+// document.getElementById('dev').innerHTML = `6/10=${num1 / num2}`;
+
+// num1 *=2  //
+// document.getElementById("min")innerHTML = num1 - num2;
+
+////////////|\\\\\\\\\\\\\\\\\\\\\\\
+
+//String
+
+document.write('zaid')
+//Number
+
+document.write('<br>')
+document.write(18)
+//Boolean
+
+document.write('<br>')
+document.write(true)
+document.write('<br>')
+document.write(false)
+//Array
+
+var zaid_info = {
+    fullname : "zaid shanan",
+    Age : 18,
+    academy : [ "A1 " , "A2 " ,"Bacalorius " , "la ilah illa allah"],
+    kayit : true
+}
+document.write('<br>')
+document.write(zaid_info .fullname);
+document.write('<br>')
+document.write(zaid_info .Age);
+document.write('<br>')
+
+document.write(zaid_info .academy);
+document.write('<br>')
+
+document.write(zaid_info .kayit);
